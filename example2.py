b1728a69eaba5555af301dd15b787f3225ad0902
Femi Jemilohun <olufemi.jemilohun@gmail.com>
2018-11-23 22:10:06 -0500

    finalized example2.py

--- example2.py
+++ example2.py
@@ -1,5 +1,7 @@
 '''
-This is an example of using the numpy library to create a matrix and print out the properties of matrix (shape, length, width, etc.). It also contains the solution to a Google interview question on 
+This is an example of using the numpy library to create a 
+matrix and print out the properties of matrix (shape, length, width, etc.).
+It also contains a tree treversal example (recursive).
 
 www.youtube.com/watch?v=IWvbPIYQPFM
 '''
@@ -36,26 +38,31 @@ for i in range(0, a.shape[0]):
 		count += 1
 print('\n')
 
-# 
+# Print the element at index (1,2)
+print('Array[1,2]: %s\n' % str(a[1][2]))
+
+# Recursively check for connected nodes
 color_count = {'blue': 0, 'red': 0, 'green': 0}
-for i in range(0, a.shape[0] - 1):
+for i in range(0, a.shape[0]):
 	for j in range(0, a.shape[1]):
-		#print(color_count[a[i][j]])
-		if j == a.shape[1]-1:
+	
+		if j < (a.shape[1] - 1) and i < (a.shape[0] - 1):
 			if a[i][j] == a[i+1][j]:
-				print('test')
+				color_count[str(a[i][j])] += 1
+			if a[i][j] == a[i][j +1]:
+				color_count[str(a[i][j])] += 1
 		
-		'''if a[i][j] == a[i+1][j]:
-			color_count[str(a[i][j])] += 1
-		if a[i][j] == a[i][j+1]:
-			color_count[str(a[i][j])] += 1'''
+		elif i == (a.shape[0] - 1) and j < (a.shape[1] - 1):
+			if a[i][j] == a[i][j+1]:
+				color_count[str(a[i][j])] += 1
+				
+		elif j == (a.shape[1] - 1) and i < (a.shape[0] - 1):
+			if a[i][j] == a[i+ 1][j]:
+				color_count[str(a[i][j])] += 1
+		
 			
-print(color_count)
-print(color_count['blue'])
-color_count['blue'] += 1
-print(color_count['blue'])
+print('Connect nodes by color: %s\n' % str(color_count))
 
 
 
 
-print(a[1][2])
