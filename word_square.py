e98a8995df9a03d29635d67a5fe30b33ec7f246f
Femi Jemilohun <olufemi.jemilohun@gmail.com>
2018-11-26 23:21:57 -0500

    Need to add functionality for a single word (ex. BOB would return true)

--- word_square.py
+++ word_square.py
@@ -20,15 +20,20 @@ def sort_list(u_list):
 
 # Function to determine if given input containts a word square
 def square_check(w_list): 
-  w_len = len(w_list[1])
+  w_len = len(w_list[0])
   ws_bool = True
+  
+  if len(w_list[0]) == 1:
+    return ('Word Square: %s\n' % ws_bool)
 
   # Exit the loop
   for r in range(0, len(w_list)):
+    # Check that all rows are of the same length
     if len(w_list[r]) != w_len:
       ws_bool = False
       break
-
+     
+    # If the array equals its transpose then we have a square
     for i in range(0, len(w_list[r])):
       if w_list[r][i] != w_list[i][r]:
         ws_bool = False
@@ -40,14 +45,18 @@ def square_check(w_list):
 	
   return ws_bool
 
+# Check if a list/sorted is a square
 square_check(list1)
 square_check(sort_list(list1))
 square_check(list2)
 square_check(sort_list(list2))
 
-per = tuple(permutations(list3, len(list3[0])))
-  
-for item in per:
+# n choose k permutation
+per = list(permutations(list3, len(list3[0])))
+
+# Iterate through the permutations and check for squares
+for item in per:  
   square_check(item)
 
-
+#square_check(['BOe'])
+square_check([''])
