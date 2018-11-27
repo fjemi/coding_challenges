350259444b8b4cff59c0963553b39fbc48f71985
Femi Jemilohun <olufemi.jemilohun@gmail.com>
2018-11-26 22:31:44 -0500

    incorporated code for part 2 of the word square challenge

--- word_square.py
+++ word_square.py
@@ -1,20 +1,14 @@
 '''
-
 Word Squares
 
- 
-
 https://techdevguide.withgoogle.com/resources/former-coding-interview-question-word-squares/#!
-
 '''
 
- 
+from itertools import permutations
 
 list1 = ['BALL', 'AREA', 'LEAD', 'LADY']
 list2 =['LADY', 'AREA', 'DEAR', 'YARD']
 list3 = ['AREA', 'BALL', 'DEAR', 'LADY', 'LEAD', 'YARD']
-
-square_list = []
 
 # Sort by first character of each word in a list
 def sort_list(u_list):
@@ -24,30 +18,42 @@ def sort_list(u_list):
     s_list.append(item)
   return s_list
 
-print(sort_list(list1))
-
 # Function to determine if given input containts a word square
 def square_check(w_list): 
-  ws_bool = False
   w_len = len(w_list[1])
   ws_bool = True
 
+  # Exit the loop
   for r in range(0, len(w_list)):
-    print(list(w_list[r]))
-
     if len(w_list[r]) != w_len:
       ws_bool = False
+      break
 
     for i in range(0, len(w_list[r])):
-        if w_list[r][i] != w_list[i][r]:
-          ws_bool = False
+      if w_list[r][i] != w_list[i][r]:
+        ws_bool = False
+        break
+          
+  if ws_bool == True:
+	  print(w_list)
+	  print('Word Square: %s\n' % ws_bool)
+	
+  return ws_bool
 
-  if ws_bool:
-    return 'This list is a word square\n'
-  else:
-    return 'This list is not a word square\n'
+square_check(list1)
+square_check(sort_list(list1))
+square_check(list2)
+square_check(sort_list(list2))
 
-print(square_check(list1))
-print(square_check(sort_list(list1)))
-print(square_check(list2))
-print(square_check(sort_list(list2)))
+per = tuple(permutations(list3, len(list3[0])))
+p_list = []
+count1 = 0
+count2 = 0
+
+'''for i in per:
+	if square_check(i):
+		print('hi')'''
+	#print('Word Square: %s\n' % square_check(i))
+		
+
+
