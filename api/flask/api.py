827f43f72d85d35bae47e5dd6b17002644df442a
Femi Jemilohun <olufemi.jemilohun@gmail.com>
2019-12-27 10:25:59 -0500

    Adding flask api

---
+++ api/flask/api.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from dataclasses import dataclass
+
+
+app = Flask(__name__)
+
+
+@dataclass
+class Hello:
+  text: str = 'Hello World!'
+
+
+@app.route('/api')
+def hello():
+  return jsonify(Hello())
+    
+    flask
+if __name__ == '__main__':
+  app.run(debug=True)
